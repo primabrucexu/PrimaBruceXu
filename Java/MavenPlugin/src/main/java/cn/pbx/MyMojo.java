11e98dacd5b961efabc68035d634e39a24a99e50
BruceXu <jiangnan.xu@tongdun.net>
2021-12-16 18:28:53 +0800

    maven插件初始化

---
+++ Java/MavenPlugin/src/main/java/cn/pbx/MyMojo.java
@@ -0,0 +1,11 @@
+package cn.pbx;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+public class MyMojo extends AbstractMojo {
+
+    public void execute() throws MojoExecutionException {
+
+    }
+}
