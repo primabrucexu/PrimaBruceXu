f7e062e1854d9c5788cb3e4f196b18d7fb0492d6
BruceXu <jiangnan.xu@tongdun.net>
2021-12-17 16:47:48 +0800

    八股文

--- Java/MavenPlugin/src/main/java/cn/pbx/MyMojo.java
+++ Java/MavenPlugin/src/main/java/cn/pbx/MyMojo.java
@@ -2,8 +2,15 @@ package cn.pbx;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
 
 public class MyMojo extends AbstractMojo {
+
+    private final Log log;
+
+    public MyMojo() {
+        log = super.getLog();
+    }
 
     public void execute() throws MojoExecutionException {
 
