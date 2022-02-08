2e5ce06fb45adeee1e3d24f09d36f72d388fdabf
BruceXu <primabrucexu@qq.com>
2022-02-08 19:06:23 +0800

    八股文

---
+++ Java/Laboratory/src/main/java/cn/pbx/Main.java
@@ -0,0 +1,16 @@
+package cn.pbx;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author BruceXu
+ * @date 2022/2/8
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1);
+        Map<String, String> map = new HashMap<>(20);
+        map.put("1", "1");
+    }
+}
