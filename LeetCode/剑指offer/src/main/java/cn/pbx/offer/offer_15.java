d9256cf4f9492237570840af8ddcdf1a4b3ac019
BruceXu <primabrucexu@qq.com>
2022-02-11 14:18:37 +0800

    剑指offer-15 二进制中1的个数

---
+++ LeetCode/剑指offer/src/main/java/cn/pbx/offer/offer_15.java
@@ -0,0 +1,33 @@
+package cn.pbx.offer;
+
+/**
+ * @author BruceXu
+ * @date 2022/2/11
+ */
+public class offer_15 {
+    public static void main(String[] args) {
+        offer_15 demo = new offer_15();
+        int n = 11;
+        System.out.println(Integer.toBinaryString(n));
+        n = 128;
+        System.out.println(Integer.toBinaryString(n));
+
+        n = -3;
+        System.out.println(Integer.toBinaryString(n));
+
+        long i = 4294967293L;
+        System.out.println((int) i);
+        System.out.println(Long.toBinaryString(i));
+    }
+
+    public int hammingWeight(int n) {
+        String s = Integer.toBinaryString(n);
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                sum++;
+            }
+        }
+        return sum;
+    }
+}
