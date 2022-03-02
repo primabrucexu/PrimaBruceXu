3785087037854ee5f589415fd2ae90da618abd65
BruceXu <695603725@qq.com>
2022-03-02 14:29:23 +0800

    剑指 Offer 21. 调整数组顺序使奇数位于偶数前面

---
+++ LeetCode/剑指offer/src/main/java/cn/pbx/offer/offer_21.java
@@ -0,0 +1,35 @@
+package cn.pbx.offer;
+
+import java.util.Arrays;
+
+/**
+ * @author BruceXu
+ * @date 2022/3/2
+ */
+public class offer_21 {
+
+    public static void main(String[] args) {
+        offer_21 demo = new offer_21();
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(demo.exchange(nums)));
+    }
+
+    public int[] exchange(int[] nums) {
+        int front = 0;
+        int back = nums.length - 1;
+        while (back - front > 1) {
+            if (nums[front] % 2 != 0) {
+                front++;
+            }
+            if (nums[back] % 2 == 0) {
+                back--;
+            }
+            if (nums[front] % 2 == 0 && nums[back] % 2 != 0) {
+                int mid = nums[front];
+                nums[front] = nums[back];
+                nums[back] = mid;
+            }
+        }
+        return nums;
+    }
+}
