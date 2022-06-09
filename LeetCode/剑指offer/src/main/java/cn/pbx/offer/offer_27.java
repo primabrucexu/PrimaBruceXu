e37affb90af6be30bb027144d2b60c5312d60dec
BruceXu <695603725@qq.com>
2022-06-09 15:27:03 +0800

    Offer 27. 二叉树的镜像

---
+++ LeetCode/剑指offer/src/main/java/cn/pbx/offer/offer_27.java
@@ -0,0 +1,28 @@
+package cn.pbx.offer;
+
+import cn.pbx.util.TreeNode;
+
+/**
+ * @author BruceXu
+ * @date 2022/6/9
+ */
+public class offer_27 {
+
+    public TreeNode mirrorTree(TreeNode root) {
+        exchangeLeftAndRight(root);
+        return root;
+    }
+
+    private void exchangeLeftAndRight(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        // 先处理子树，再处理子树的根
+        exchangeLeftAndRight(node.left);
+        exchangeLeftAndRight(node.right);
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+    }
+
+}
